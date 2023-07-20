4e4b3ffb7f3f98f5199a8ca72285619e34f30c79
waytoreal <waytoreal18@gmail.com>
2023-07-20 23:19:28 +0900

    fourth

--- code/player.py
+++ code/player.py
@@ -1,5 +1,6 @@
 import pygame
 from setting import *
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
@@ -7,6 +8,9 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0,-20)
+
+        # graphics setup
+        self.import_player_assets()
 
         # movement
         self.direction = pygame.math.Vector2()
@@ -22,6 +26,11 @@ class Player(pygame.sprite.Sprite):
         self.animations ={'up':[], 'down':[], 'left':[], 'right':[],
         'right_idle':[], 'left_idle':[], 'up_idle':[],'down_idle':[],
         'right_attack':[],'left_attack':[],'up_attack':[], 'down_attack':[]}
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+       
 
     def input(self):
         keys = pygame.key.get_pressed()
